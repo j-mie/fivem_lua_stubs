f307fbdf97b8b812280bde5bb37dafc4abd89d5e
Richard Goodman <richardg@brandwatch.com>
2020-05-24 18:44:57 +0100

    Remove __init__.py and constants.py for Jamie

--- stub_generator.py
+++ stub_generator.py
@@ -3,13 +3,43 @@ import string
 
 import requests
 
-from constants import (
-    BLACKLISTED_PARAMS,
-    DOC_URLS,
-    TYPE_TRANSLATION_TABLE
-)
 
-
+BLACKLISTED_PARAMS = [
+    "and",
+    "end",
+    "in",
+    "repeat",
+    "break",
+    "false",
+    "local",
+    "return",
+    "do",
+    "for",
+    "nil",
+    "then",
+    "else",
+    "function",
+    "not",
+    "true",
+    "elseif",
+    "if",
+    "or",
+    "until",
+    "while"
+]
+DOC_URLS = [
+    "https://runtime.fivem.net/doc/natives.json",
+    "https://runtime.fivem.net/doc/natives_cfx.json"
+]
+TYPE_TRANSLATION_TABLE = {
+    "float": "number",
+    "int": "number",
+    "bool": "boolean",
+    "BOOL": "boolean",
+    "Any": "any",
+    "char": "string",
+    "Vector3": "vector3"
+}
 LINES_WRITTEN_CAP = 1000
 PREFIX = "stubs/natives"
 
